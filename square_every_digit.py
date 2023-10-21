97315283b17347b4fcd120b8ba7a922260169a8e
Engramar Bollas <engramar.bollas@gmail.com>
2023-10-21 21:46:51 +1100

    Square every digit

---
+++ square_every_digit.py
@@ -0,0 +1,9 @@
+def square_digits(num):
+    output=''    
+    for digit in str(num):   
+      squared = int(digit) ** 2      
+      output += str(squared)   
+    return int(output)
+    print(output)   
+
+square_digits(0)
